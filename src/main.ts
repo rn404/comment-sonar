88919c1ca8ad449b8f18e66500930bc76cd762a1
rn404 <nekonekonekona@gmail.com>
2025-04-27 20:05:56 +0900

    Exec `deno task precommit`

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { scanTodos } from "./scanner.ts";
-import { createOrUpdateIssue } from "./github.ts";
-import { Logger } from "./logger.ts";
+import { scanTodos } from './scanner.ts'
+import { createOrUpdateIssue } from './github.ts'
+import { Logger } from './logger.ts'
 
 async function main() {
-  Logger.message("Scanning for TODO/FIXME comments...");
-  const todos = await scanTodos(".");
+  Logger.message('Scanning for TODO/FIXME comments...')
+  const todos = await scanTodos('.')
 
-  Logger.message(`Number of comments detected: ${todos.length}`);
-  
-  if (Deno.env.get("GITHUB_ACTIONS")) {
-    await createOrUpdateIssue(todos);
+  Logger.message(`Number of comments detected: ${todos.length}`)
+
+  if (Deno.env.get('GITHUB_ACTIONS')) {
+    await createOrUpdateIssue(todos)
   } else {
-    Logger.message("Running locally, so no issues will be created.");
-    Logger.message(todos);
+    Logger.message('Running locally, so no issues will be created.')
+    Logger.message(todos)
   }
 }
 
-main().catch(console.error);
+main().catch(console.error)
