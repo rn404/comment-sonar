4d5d210667ef4986b1013a2a50ed18362004fec5
rn404 <nekonekonekona@gmail.com>
2025-04-28 00:21:22 +0900

    Exec `deno task precommit`

--- src/github_test.ts
+++ src/github_test.ts
@@ -5,9 +5,9 @@ import { GithubIssueClient } from './github.ts'
 Deno.test('GithubIssueClient: checkExistingIssue finds an issue', async () => {
   const fetchStub = stub(globalThis, 'fetch', () =>
     Promise.resolve({
-      json: () => Promise.resolve([{ title: ':pushpin: TODO/FIXME List', number: 123 }]),
-    } as Response),
-  )
+      json: () =>
+        Promise.resolve([{ title: ':pushpin: TODO/FIXME List', number: 123 }]),
+    } as Response))
 
   try {
     const client = new GithubIssueClient('test-token', 'owner/repo')
@@ -19,8 +19,10 @@ Deno.test('GithubIssueClient: checkExistingIssue finds an issue', async () => {
 })
 
 Deno.test('GithubIssueClient: update modifies an existing issue', async () => {
-  const fetchStub = stub(globalThis, 'fetch', () =>
-    Promise.resolve({ ok: true } as Response),
+  const fetchStub = stub(
+    globalThis,
+    'fetch',
+    () => Promise.resolve({ ok: true } as Response),
   )
 
   try {
@@ -28,7 +30,10 @@ Deno.test('GithubIssueClient: update modifies an existing issue', async () => {
     await client['update'](123, ['TODO: Test case 1', 'FIXME: Test case 2'])
 
     assertEquals(fetchStub.calls.length, 1)
-    assertEquals(fetchStub.calls[0].args[0], 'https://api.github.com/repos/owner/repo/issues/123')
+    assertEquals(
+      fetchStub.calls[0].args[0],
+      'https://api.github.com/repos/owner/repo/issues/123',
+    )
     assertEquals(fetchStub.calls[0].args[1]?.method, 'PATCH')
   } finally {
     fetchStub.restore()
@@ -40,16 +45,21 @@ Deno.test('GithubIssueClient: create creates a new issue', async () => {
     Promise.resolve({
       ok: true,
       json: () => Promise.resolve({ number: 456 }),
-    } as Response),
-  )
+    } as Response))
 
   try {
     const client = new GithubIssueClient('test-token', 'owner/repo')
-    const result = await client['create'](['TODO: Test case 1', 'FIXME: Test case 2'])
+    const result = await client['create']([
+      'TODO: Test case 1',
+      'FIXME: Test case 2',
+    ])
 
     assertEquals(result, { number: 456 })
     assertEquals(fetchStub.calls.length, 1)
-    assertEquals(fetchStub.calls[0].args[0], 'https://api.github.com/repos/owner/repo/issues')
+    assertEquals(
+      fetchStub.calls[0].args[0],
+      'https://api.github.com/repos/owner/repo/issues',
+    )
     assertEquals(fetchStub.calls[0].args[1]?.method, 'POST')
   } finally {
     fetchStub.restore()
@@ -60,7 +70,11 @@ Deno.test('GithubIssueClient: exec updates an existing issue if found', async ()
   const fetchStub = stub(globalThis, 'fetch', (url) => {
     if (url.toString().includes('state=open')) {
       return Promise.resolve({
-        json: () => Promise.resolve([{ title: ':pushpin: TODO/FIXME List', number: 123 }]),
+        json: () =>
+          Promise.resolve([{
+            title: ':pushpin: TODO/FIXME List',
+            number: 123,
+          }]),
       } as Response)
     }
     return Promise.resolve({ ok: true } as Response)
@@ -71,8 +85,14 @@ Deno.test('GithubIssueClient: exec updates an existing issue if found', async ()
     await client.exec(['TODO: Test case 1', 'FIXME: Test case 2'])
 
     assertEquals(fetchStub.calls.length, 2)
-    assertEquals(fetchStub.calls[0].args[0], 'https://api.github.com/repos/owner/repo/issues?state=open&labels=TODO')
-    assertEquals(fetchStub.calls[1].args[0], 'https://api.github.com/repos/owner/repo/issues/123')
+    assertEquals(
+      fetchStub.calls[0].args[0],
+      'https://api.github.com/repos/owner/repo/issues?state=open&labels=TODO',
+    )
+    assertEquals(
+      fetchStub.calls[1].args[0],
+      'https://api.github.com/repos/owner/repo/issues/123',
+    )
   } finally {
     fetchStub.restore()
   }
@@ -94,8 +114,14 @@ Deno.test('GithubIssueClient: exec creates a new issue if none exists', async ()
     await client.exec(['TODO: Test case 1', 'FIXME: Test case 2'])
 
     assertEquals(fetchStub.calls.length, 2)
-    assertEquals(fetchStub.calls[0].args[0], 'https://api.github.com/repos/owner/repo/issues?state=open&labels=TODO')
-    assertEquals(fetchStub.calls[1].args[0], 'https://api.github.com/repos/owner/repo/issues')
+    assertEquals(
+      fetchStub.calls[0].args[0],
+      'https://api.github.com/repos/owner/repo/issues?state=open&labels=TODO',
+    )
+    assertEquals(
+      fetchStub.calls[1].args[0],
+      'https://api.github.com/repos/owner/repo/issues',
+    )
   } finally {
     fetchStub.restore()
   }
